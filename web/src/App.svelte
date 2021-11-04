c05cc65f3162abe4cb202324ea9e18ed63f0e11b
Ford, Sam Michael <samford@sams-macbook-pro.local>
2021-11-04 18:50:18 -0400

    Svelte website built

---
+++ web/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  let todos = [];
+  fetch("https://cjo5jmgvm3.execute-api.us-east-1.amazonaws.com/Prod/todos/")
+            .then((resp) => {
+							return resp.json();
+						})
+						.then((data) => {
+							todos = data;
+						})
+</script>
+
+<h1>Todo List</h1>
+<ul>
+  {#each todos as todo}
+    <li>
+      <span>{todo.desc}</span>
+    </li>
+  {/each}
+</ul>
